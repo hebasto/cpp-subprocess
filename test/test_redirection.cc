368db5c4829471d8fa4551254ec2daea36fe8043
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-12-01 16:53:24 +0000

    test: Add cross-platform test for error redirection

---
+++ test/test_redirection.cc
@@ -0,0 +1,22 @@
+#include <string>
+#include <tuple>
+#include <subprocess.hpp>
+
+using namespace subprocess;
+
+void test_redirection(const std::string& command)
+{
+  auto p = Popen(command, output{PIPE}, error{PIPE});
+  OutBuffer out;
+  ErrBuffer err;
+  std::tie(out, err) = p.communicate();
+  std::cout << out.buf.data() << std::endl;
+  std::cout << err.buf.data() << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  std::string command{argv[0]};
+  command += ".py";
+  test_redirection(command);
+  return 0;
+}
